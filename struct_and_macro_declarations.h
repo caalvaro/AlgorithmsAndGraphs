5117206c337558f589044d4c4075670bdc48179e
CAAlvaro <alvarocarvalho@live.com>
2020-03-23 01:01:45 -0300

    Added declarations for structs and macros to represent Graphs

---
+++ struct_and_macro_declarations.h
@@ -0,0 +1,45 @@
+#ifndef STRUCT_AND_MACRO_DECLARATIONS_H_INCLUDED
+#define STRUCT_AND_MACRO_DECLARATIONS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define WHITE 0
+#define GRAY 1
+#define BLACK 2
+
+#define INFINITY -1
+
+#define FALSE 0
+#define TRUE 1
+
+typedef struct _VERTEX VERTEX;
+typedef struct _LIST_NODE LIST_NODE;
+typedef struct _LIST_HEAD LIST_HEAD;
+typedef struct _GRAPH GRAPH;
+
+struct _VERTEX {
+    int name;
+    int color;
+    int distance;
+    int degree;
+    VERTEX* father;
+};
+
+struct _LIST_NODE {
+    VERTEX* vertex;
+    LIST_NODE* next_node;
+};
+
+struct _LIST_HEAD {
+    LIST_NODE* first_node;
+};
+
+struct _GRAPH {
+    int number_vertices;
+    int number_edges;
+    int is_digraph;
+    LIST_HEAD* adjacency_list;
+};
+
+#endif
