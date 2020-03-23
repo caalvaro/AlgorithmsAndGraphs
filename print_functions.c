b9bb46948d116b38add4dba1030d9ffd8e793f93
CAAlvaro <alvarocarvalho@live.com>
2020-03-23 01:04:46 -0300

    Added a function to print the adjacency list

---
+++ print_functions.c
@@ -0,0 +1,27 @@
+#include "print_functions.h"
+
+extern void print_adjacency_list(LIST_HEAD* adjacency_list, int number_vertices) {
+    int i;
+    LIST_NODE* aux_node;
+
+    printf("\n");
+
+    for (i = 0; i < number_vertices; i++) {
+        printf("VERTEX %d: ", i);
+
+        aux_node = (adjacency_list + i)->first_node;
+
+        if (!aux_node) {
+            printf("*");
+        }
+
+        while (aux_node) {
+            printf("%d ", aux_node->vertex->name);
+            aux_node = aux_node->next_node;
+        }
+
+        printf("\n");
+    }
+
+    printf("\n");
+}
