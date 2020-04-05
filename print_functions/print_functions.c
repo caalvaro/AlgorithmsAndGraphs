fd7a75a69ec8826dfcb4411195c829db36a67a2e
CAAlvaro <alvarocarvalho@live.com>
2020-04-04 22:53:00 -0300

    Added function to print vertices

--- print_functions/print_functions.c
+++ print_functions/print_functions.c
@@ -25,3 +25,31 @@ extern void print_adjacency_list(LIST_HEAD* adjacency_list, int number_vertices)
 
     printf("\n");
 }
+
+extern void print_vertices_list(VERTEX* vertices_list, int number_vertices) {
+    VERTEX* vertex;
+    int i;
+
+    printf("\n***VERTICES LIST***\n");
+
+    for (i = 0; i < number_vertices; i++) {
+        int father_name;
+        char* color;
+
+        vertex = vertices_list + i;
+        if (vertex->father) father_name = vertex->father->name;
+        else father_name = 0;
+
+        if (vertex->color == 0) color = "WHITE";
+        else if (vertex->color == 1) color = "GRAY";
+        else if (vertex->color == 2) color = "BLACK";
+
+        printf("VERTEX %d:\n\tColor: %s\n\tDistance: %d\n\tFather: %d\n",
+            vertex->name,
+            color,
+            vertex->distance,
+            father_name);
+    }
+
+    printf("\n*******************\n");
+}
--- print_functions/print_functions.h
+++ print_functions/print_functions.h
@@ -1,3 +1,4 @@
 #include "../struct_and_macro_declarations.h"
 
 extern void print_adjacency_list(LIST_HEAD* adjacency_list, int number_vertices);
+extern void print_vertices_list(VERTEX* vertices_list, int number_vertices);
